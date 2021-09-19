46a6c2a75b352304e12e7e33629531828f491704
Tân <ngoctana1k2014@gmail.com>
2021-09-19 22:53:35 +0700

    update file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,35 @@
 import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
 import data from './data'
+import Product from './components/Product'
 
 function App() {
     return (
-        <div className="grid-container">
-            <header className="row">
-                <div>
-                    <a className="brand" href="/">Amazona</a>
-                </div>
-                <div>
-                    <a href="/cart">Cart</a>
-                    <a href="/signin">Sign In</a>
-                </div>
-            </header>
-            <main>
-                <div className="row center">
-                    {
-                        data.products.map(product => (
-                            <div key={product._id} className="card">
-                                <a href={`/product/${product._id}`}>
-                                    <img className="medium" src={product.image} alt={product.name} />
-                                </a>
-                                <div className="card-body">
-                                    <a href={`/product/${product._id}`}>
-                                        <h2>{product.name}</h2>
-                                    </a>
-                                    <div className="rating">
-                                        <span><i className="fa fa-star"></i></span>
-                                        <span><i className="fa fa-star"></i></span>
-                                        <span><i className="fa fa-star"></i></span>
-                                        <span><i className="fa fa-star"></i></span>
-                                        <span><i className="fa fa-star"></i></span>
-                                    </div>
-                                    <div className="price">${product.price}</div>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
-            </main>
-            <footer className="row center">
-                &#169; Tan. All rights reserved
-            </footer>
-        </div>
+        <BrowserRouter>
+            <div className="grid-container">
+                <header className="row">
+                    <div>
+                        <a className="brand" href="/">Amazona</a>
+                    </div>
+                    <div>
+                        <a href="/cart">Cart</a>
+                        <a href="/signin">Sign In</a>
+                    </div>
+                </header>
+                <main>
+                    <div className="row center">
+                        {
+                            data.products.map(product => (
+                                <Product key={product._id} product={product}></Product>
+                            ))
+                        }
+                    </div>
+                </main>
+                <footer className="row center">
+                    &#169; Tan. All rights reserved
+                </footer>
+            </div>
+        </BrowserRouter>
     )
 }
 
